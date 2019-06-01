f553eee7d10df3dac4b86298dae6111861f381c7
Tomiqua <tomiqua@gmail.com>
2019-06-01 10:59:57 -0400

    2nd test hello

--- homework.js
+++ homework.js
@@ -3,3 +3,5 @@ const app= express ();
 app.listen(3000, function (){
     console.log ('Homework app listening on port 3000');});
     app.get('/', (req, res) => res.send('This is my CodeSquad homework.'));
+
+    //hello
