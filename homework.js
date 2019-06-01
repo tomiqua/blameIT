fd6d4bcd7bcef550ccfe29c1455731389219cb1b
Tomiqua <tomiqua@gmail.com>
2019-06-01 10:49:51 -0400

    test commit

---
+++ homework.js
@@ -0,0 +1,5 @@
+const express= require('express');
+const app= express ();
+app.listen(3000, function (){
+    console.log ('Homework app listening on port 3000');});
+    app.get('/', (req, res) => res.send('This is my CodeSquad homework.'));
